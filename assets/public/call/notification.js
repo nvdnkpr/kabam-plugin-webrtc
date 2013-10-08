fc20b68180937e579a28c61afc4e01cbe8753d9d
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-10-08 23:25:07 +0700

    style(assets): reindent the the notification.js

--- assets/public/call/notification.js
+++ assets/public/call/notification.js
@@ -1,9 +1,9 @@
 if (typeof io !== 'undefined') {
   var socket = io.connect();
-  socket.on('broadcast', function (data) {
-      $('#clock').html(data.time);
+  socket.on('broadcast', function(data) {
+    $('#clock').html(data.time);
   });
-  socket.on('notify', function (data) {
-      $('#flash').append('<div class="alert alert-info"><a class="close" data-dismiss="alert">×</a><strong>'+data.message+'</strong></div>');
+  socket.on('notify', function(data) {
+    $('#flash').append('<div class="alert alert-info"><a class="close" data-dismiss="alert">×</a><strong>' + data.message + '</strong></div>');
   });
 }
